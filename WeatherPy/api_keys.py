478c3c156aa6766e621401360f794d48e750f0b2
CoriEsposito <coriesposito1@gmail.com>
2025-03-12 13:18:40 -0400

    updated files

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "00352a25ef1826a6ba740da7d6eb7667"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "c2afb099ba92469784a2f48a6da2430a"
